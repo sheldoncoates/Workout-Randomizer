3b29feea9a0fc793a592c4e0d6776f4ba8a86836
sheldoncoates <sjrcoates@sympatico.ca>
2019-05-21 16:14:23 -0400

    Added search all exercises functionality and a license for distribution

--- src/screens/AllExercisesScreen.js
+++ src/screens/AllExercisesScreen.js
@@ -1,52 +1,84 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, ActivityIndicator } from 'react-native';
-import styles from '../Styles/AllExercisesScreenStyle';
+import { View, Text, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
+// import styles from '../Styles/AllExercisesScreenStyle';
 import { SearchBar } from 'react-native-elements';
-import { ScrollView } from 'react-native-gesture-handler';
+import { BackData } from '../Data/BackData.js';
+import { BicepData } from '../Data/BicepData.js';
+import { ChestData } from '../Data/ChestData.js';
+import { TricepData } from '../Data/TricepData.js';
+import { LegData } from '../Data/LegData.js';
+import { ShoulderData } from '../Data/ShoulderData.js';
+import { AbdomenData } from '../Data/AbdomenData.js';
 
-//****** Folllow this tutoiral for search */ https://aboutreact.com/searching-in-list-using-search-bar/
 export default class AllExercisesScreen extends Component {
-    constructor(props) {
-        super(props);
-        //setting default state
-        this.state = { isLoading: true, search: '' };
-        this.arrayholder = [];
+  constructor(props) {
+    super(props);
+    //setting default state
+    this.state = { isLoading: true,dataSource: [], search: '' };
+    this.arrayholder = [];
+  }
+  componentDidMount() {
+    this.arrayholder = [...BackData, ...BicepData, ...ChestData, ...TricepData, ...LegData, ...ShoulderData, ...AbdomenData];
+    this.state.dataSource = this.arrayholder;
+    this.setState({
+      isLoading: false
+    });
+  }
+  SearchFilterFunction(text) {
+    //passing the inserted text in textinput
+    const newData = this.arrayholder.filter(function (item) {
+      //applying filter for the inserted text in search bar
+      const itemData = item.Name ? item.Name.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      //setting the filtered newData on datasource
+      //After setting the data it will automatically re-render the view
+      dataSource: newData,
+      search: text,
+    });
+  };
+  render() {  
+    if (this.state.isLoading) {
+      //Loading View while data is loading
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
     }
-    componentDidMount() {
-        return fetch('https://jsonplaceholder.typicode.com/posts')
-          .then(response => response.json())
-          .then(responseJson => {
-            this.setState(
-              {
-                isLoading: false,
-                dataSource: responseJson,
-              },
-              function() {
-                this.arrayholder = responseJson;
-              }
-            );
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      }
-    updateSearch = search => {
-        this.setState({ search });
-    };
-    render() {
+    return (
+      <View style={styles.viewStyle}>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={text => this.SearchFilterFunction(text)}
+          onClear={text => this.SearchFilterFunction('')}
+          value={this.state.search}
+          lightTheme={true}
+        />
+        <FlatList
+          data={this.state.dataSource}
+          // ItemSeparatorComponent={this.ListViewItemSeparator}
+          renderItem={({ item }) => (
+            <Text style={styles.textStyle}>{item.Name}</Text>
+          )}
+          enableEmptySections={true}
+          style={{ marginTop: 10 }}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
 
-        const { search } = this.state;
-
-        return (
-            <ScrollView>
-                <SearchBar
-                    placeholder="Type Here..."
-                    onChangeText={this.updateSearch}
-                    value={search}
-                    lightTheme={true}
-                />
-                <Text>This doesn't work currently, just go back</Text>
-            </ScrollView>
-        );
-    }
+    );
+  }
 }
+const styles = StyleSheet.create({
+  viewStyle: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  textStyle: {
+    padding: 10,
+  },
+});
